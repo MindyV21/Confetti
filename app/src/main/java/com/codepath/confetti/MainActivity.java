99d80cc9001264861b71776a1b9dc9e081c64721
Mindy Van <mmv0321@gmail.com>
2021-07-13 14:59:39 -0500

    create layout for bottom navigation view

--- app/src/main/java/com/codepath/confetti/MainActivity.java
+++ app/src/main/java/com/codepath/confetti/MainActivity.java
@@ -1,20 +1,29 @@
 package com.codepath.confetti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.codepath.confetti.databinding.ActivityMainBinding;
 import com.codepath.confetti.databinding.ActivitySignUpBinding;
+import com.codepath.confetti.fragments.NotesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         mAuth = FirebaseAuth.getInstance();
-        Log.i(TAG, "" + mAuth.getCurrentUser());
+        Log.i(TAG, "" + mAuth.getCurrentUser().getDisplayName());
+
+        bottomNavigationView = binding.bottomNavigation;
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new NotesFragment();
+                        break;
+                    case R.id.action_upload:
+//                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+//                        fragment = new InstaProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     public void logout() {
