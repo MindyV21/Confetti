7507daa56b34d8c703552602b5c1d65729cf1d9f
Mindy Van <mmv0321@gmail.com>
2021-07-13 11:21:38 -0500

    basic input field logic for login and signup activities

--- app/src/main/java/com/codepath/confetti/LoginActivity.java
+++ app/src/main/java/com/codepath/confetti/LoginActivity.java
@@ -4,9 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.confetti.databinding.ActivityLoginBinding;
@@ -20,6 +26,13 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     public static final String TAG = "LoginActivity";
+    public static final int VALID = 1;
+
+    private ImageView ivLogo;
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,75 @@ public class LoginActivity extends AppCompatActivity {
 
         // init firebase auth
         mAuth = FirebaseAuth.getInstance();
+
+        // init layout
+        ivLogo = binding.ivLogo;
+        etEmail = binding.etEmail;
+        etPassword = binding.etPassword;
+        btnLogin = binding.btnLogin;
+        tvSignUp = binding.tvSignUp;
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: improve input checks
+                // check input values
+                if (checkEmailInput() != VALID) return;
+
+                if (checkPasswordInput() != VALID) return;
+
+
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignUpActivity();
+            }
+        });
+    }
+
+    private void goToSignUpActivity() {
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    // returns VALID if valid password, 0 otherwise
+    private int checkPasswordInput() {
+        String password = etPassword.getText().toString().trim();
+        if (password.isEmpty()) {
+            Log.i(TAG, "Password is empty");
+            etPassword.setError("Password is required!");
+            etPassword.requestFocus();
+            return 0;
+        }
+        return VALID;
+    }
+
+    private int checkEmailInput() {
+        String email = etEmail.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            Log.i(TAG, "Email is empty");
+            etEmail.setError("Email is required!");
+            etEmail.requestFocus();
+            return 0;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Log.i(TAG, "Email is invalid");
+            etEmail.setError("Please provide valid email!");
+            etEmail.requestFocus();
+            return 0;
+        }
+
+        return VALID;
     }
 
     @Override
--- app/src/main/java/com/codepath/confetti/SignUpActivity.java
+++ app/src/main/java/com/codepath/confetti/SignUpActivity.java
@@ -3,9 +3,14 @@ package com.codepath.confetti;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.codepath.confetti.databinding.ActivityLoginBinding;
@@ -20,6 +25,14 @@ public class SignUpActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     public static final String TAG = "SignUpActivity";
+    public static final int VALID = 1;
+
+    private ImageView ivProfile;
+    private EditText etFullName;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etConfirmPassword;
+    private Button btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,98 @@ public class SignUpActivity extends AppCompatActivity {
 
         // init firebase auth
         mAuth = FirebaseAuth.getInstance();
+
+        // init layout
+        ivProfile = binding.ivProfile;
+        etFullName = binding.etFullName;
+        etEmail = binding.etEmail;
+        etPassword = binding.etPassword;
+        etConfirmPassword = binding.etConfirmPassword;
+        btnCreateAccount = binding.btnCreateAccount;
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: improve input checks
+                // check input values
+                if (checkNameInput() != VALID) return;
+                if (checkEmailInput() != VALID) return;
+                if (checkPasswordsInput() != VALID) return;
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    private int checkPasswordsInput() {
+        String password = etPassword.getText().toString().trim();
+        String confirmPassword = etConfirmPassword.getText().toString().trim();
+
+        if (password.isEmpty()) {
+            Log.i(TAG, "Password is empty");
+            etPassword.setError("Password is required!");
+            etPassword.requestFocus();
+            return 0;
+        }
+
+        if (confirmPassword.isEmpty()) {
+            Log.i(TAG, "Confirm Password is empty");
+            etConfirmPassword.setError("Confirm password is required!");
+            etConfirmPassword.requestFocus();
+            return 0;
+        }
+
+        if (password.length() < 8) {
+            Log.i(TAG, "Password length < 8");
+            etPassword.setError("Password length must be greater than 8!");
+            etPassword.requestFocus();
+            return 0;
+        }
+
+        if (password.equals(confirmPassword)) {
+            Log.i(TAG, "Password != Confirm password");
+            etConfirmPassword.setError("Passwords do not match!");
+            etConfirmPassword.requestFocus();
+            return 0;
+        }
+
+        return VALID;
+    }
+
+    private int checkNameInput() {
+        String fullName = etFullName.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            Log.i(TAG, "Full Name is empty");
+            etFullName.setError("Full name is required!");
+            etFullName.requestFocus();
+            return 0;
+        }
+
+        return VALID;
+    }
+
+    private int checkEmailInput() {
+        String email = etEmail.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            Log.i(TAG, "Email is empty");
+            etEmail.setError("Email is required!");
+            etEmail.requestFocus();
+            return 0;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Log.i(TAG, "Email is invalid");
+            etEmail.setError("Please provide valid email!");
+            etEmail.requestFocus();
+            return 0;
+        }
+
+        return VALID;
     }
 
     @Override
