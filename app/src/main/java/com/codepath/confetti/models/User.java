561e9d51fa074aa937d4dd032481976d79354f12
Mindy Van <mmv0321@gmail.com>
2021-07-13 11:27:53 -0500

    filled out user model, added progress bar

--- app/src/main/java/com/codepath/confetti/models/User.java
+++ app/src/main/java/com/codepath/confetti/models/User.java
@@ -1,4 +1,29 @@
 package com.codepath.confetti.models;
 
 public class User {
+
+    private String fullName, email;
+
+    public User() {}
+
+    public User(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
